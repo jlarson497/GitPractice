17179a2a0873b8c3b2d7ba7a1fa4872d74d87ba4
jlarson497 <kf2617gi@go.minneapolis.edu>
2015-02-24 13:29:54 -0600

    Added new writeline

--- GitPractice/Module1.vb
+++ GitPractice/Module1.vb
@@ -3,6 +3,7 @@
     Sub Main()
         Console.WriteLine("This is a test")
         Console.WriteLine("This is a new version of the test")
+        Console.WriteLine("This is the newest version of the test")
 
     End Sub
 
