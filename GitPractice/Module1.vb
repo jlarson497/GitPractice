1ced271bc6d5946217ac58c1d7ba2b2898c0c1f4
jlarson497 <kf2617gi@go.minneapolis.edu>
2015-02-24 13:39:50 -0600

    Committing to new branch

--- GitPractice/Module1.vb
+++ GitPractice/Module1.vb
@@ -4,6 +4,7 @@
         Console.WriteLine("This is a test")
         Console.WriteLine("This is a new version of the test")
         Console.WriteLine("This is the newest version of the test")
+        Console.WriteLine("This is a new commit in a test branch")
         Console.ReadKey()
 
     End Sub
